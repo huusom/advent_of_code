d99a90349d6619e8de95e8a7ca3899dcd00e31cd
Christian Frank Huusom <huusom@huusom.org>
2019-12-01 21:00:27 +0100

    Added missing code.

--- 2019/day-01.fsx
+++ 2019/day-01.fsx
@@ -1,7 +1,18 @@
 open System.IO
 
-Path.Combine(__SOURCE_DIRECTORY__, __SOURCE_FILE__.Replace("fsx", "txt"))
+let calculate_1 i = i / 3 - 2
+
+let rec calculate_2 i =
+    let chooser x = if x <= 0 then None else Some (x, x)
+    Seq.unfold (calculate_1 >> chooser) i
+    |> Seq.sum
+
+let eval f = 
+    Path.Combine(__SOURCE_DIRECTORY__, __SOURCE_FILE__.Replace("fsx", "txt"))
     |> File.ReadLines
     |> Seq.map (int)
-    |> Seq.map (fun i -> (i / 3) - 2)
+    |> Seq.map f
     |> Seq.sum
+
+let answer_1 = eval calculate_1
+let answer_2 = eval calculate_2
