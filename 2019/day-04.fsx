7f3dde312db376715b4b4ca91196840d53809a50
huusom <huusom@huusom.org>
2019-12-04 10:04:14 +0100

    Solved day 4

--- 2019/day-04.fsx
+++ 2019/day-04.fsx
@@ -11,21 +11,25 @@ let toPairs n =
 let isIncreasing (a, b) = a <= b
 let isEqual (a, b) = a = b
 
-let isValid i =
-    let n =  sprintf "%i" i
+
+let rule1 i =
+    let n = sprintf "%i" i
     let p = toPairs n
     let x = p |> Seq.forall isIncreasing
     let y = p |> Seq.exists isEqual
     x && y
 
+let rule2 i =
+    sprintf "%i" i
+    |> toPairs
+    |> Seq.filter isEqual
+    |> Seq.groupBy id   
+    |> Seq.map (snd >> Seq.length)
+    |> Seq.exists ((=) 1)
+
 let a =
-    seq {
-        for n in 272091 .. 815432 do
-            isValid n
-    }
-    |> Seq.filter id
+    [ 272091 .. 815432 ]
+    |> Seq.filter rule1
+    |> Seq.filter rule2
     |> Seq.length
-
-
- tokenize (Regex @"(\d)\1") "111122"
 
