335560c8447fc5aebb91b38401699e5fef207779
Christian Frank Huusom <huusom@huusom.org>
2019-12-05 19:08:08 +0100

    day-05 complete

--- 2019/IntCode.fs
+++ 2019/IntCode.fs
@@ -1,76 +1,135 @@
 module IntCode
 
-type Program = {
-    Pointer : int
-    Memory : int []
-}
-
 type Parameter =
     | Position of int
     | Immediate of int
 
-type  Op = 
+type Program =
+    { Pointer: int
+      Args: int list
+      Memory: int [] }
+
+
+type Op =
     | Halt
     | Add of Parameter * Parameter * Parameter
     | Mult of Parameter * Parameter * Parameter
     | Input of Parameter
     | Output of Parameter
+    | JumpIfTrue of Parameter * Parameter
+    | JumpIfFalse of Parameter * Parameter
+    | LessThan of Parameter * Parameter * Parameter
+    | Equals of Parameter * Parameter * Parameter
 
-module Program = 
-    open System
-    open AdventOfCode
 
-    let Empty = {
-        Pointer = 0
-        Memory = [| 0 |]
-    }
+let Empty =
+    { Pointer = 0
+      Args = []
+      Memory = [| 0 |] }
+
+let peek index program = program.Memory.[program.Pointer + index]
+
+let parameterOf program index =
+    function
+    | '1' ->
+        program
+        |> peek index
+        |> Immediate
+    | _ ->
+        program
+        |> peek index
+        |> Position
+
+let parameter1 program c = parameterOf program 1 c
+let parameter2 program (c1, c2) = parameterOf program 1 c1, parameterOf program 2 c2
+let parameter3 program (c1, c2, c3) = parameterOf program 1 c1, parameterOf program 2 c2, parameterOf program 3 c3
+
+let read program =
+    function
+    | Immediate v -> v
+    | Position v -> program.Memory.[v]
+
+let write program parameter v =
+    match parameter with
+    | Immediate p -> program.Memory.[p] <- v
+    | Position p -> program.Memory.[p] <- v
+
+let eval program =
+    let instruction =
+        program
+        |> peek 0
+        |> fun i -> i.ToString().PadLeft(5, '0')
+        |> Seq.toList
+        |> List.rev
+    match instruction with
+    | '1' :: _ :: m1 :: m2 :: m3 :: _ -> parameter3 program (m1, m2, m3) |> Add
+    | '2' :: _ :: m1 :: m2 :: m3 :: _ -> parameter3 program (m1, m2, m3) |> Mult
+    | '3' :: _ :: m1 :: _ -> parameter1 program m1 |> Input
+    | '4' :: _ :: m1 :: _ -> parameter1 program m1 |> Output
+    | '5' :: _ :: m1 :: m2 :: _ -> parameter2 program (m1, m2) |> JumpIfTrue
+    | '6' :: _ :: m1 :: m2 :: _ -> parameter2 program (m1, m2) |> JumpIfFalse
+    | '7' :: _ :: m1 :: m2 :: m3 :: _ -> parameter3 program (m1, m2, m3) |> LessThan
+    | '8' :: _ :: m1 :: m2 :: m3 :: _ -> parameter3 program (m1, m2, m3) |> Equals
+    | _ -> Halt
+
+let exec program =
+    function
+    | Halt -> program
+    | Add(p1, p2, p3) ->
+        write program p3 ((read program p1) + (read program p2))
+        { program with Pointer = program.Pointer + 4 }
+    | Mult(p1, p2, p3) ->
+        write program p3 ((read program p1) * (read program p2))
+        { program with Pointer = program.Pointer + 4 }
+    | Output p ->
+        printfn "%i" (read program p)
+        { program with Pointer = program.Pointer + 2 }
+    | Input p ->
+        write program p (program.Args.Head)
+        { program with
+              Pointer = program.Pointer + 2
+              Args = program.Args.Tail }
+    | Equals(p1, p2, p3) ->
+        let v =
+            if (read program p2) = (read program p1) then 1
+            else 0
+        write program p3 v
+        { program with Pointer = program.Pointer + 4 }
+    | JumpIfFalse(p1, p2) ->
+        let p =
+            if (read program p1) = 0 then read program p2
+            else program.Pointer + 3
+        { program with Pointer = p }
+    | JumpIfTrue(p1, p2) ->
+        let p =
+            if (read program p1) <> 0 then read program p2
+            else program.Pointer + 3
+        { program with Pointer = p }
+    | LessThan(p1, p2, p3) ->
+        let v =
+            if (read program p1) < (read program p2) then 1
+            else 0
+        write program p3 v
+        { program with Pointer = program.Pointer + 4 }
+
+
+let rec run program =
+    let o = eval program
+    match o with
+    | Halt -> program
+    | _ -> exec program o |> run
+
+let runWitArgs program args = run { program with Args = args }
+
+let load (source: string) =
+    let m = source.Split([| ',' |]) |> Array.map (int)
+    { Empty with Memory = m }
+
+let p =
+    load
+        "3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99"
     
-    let peek index program = program.Memory.[program.Pointer + index]
-
-    let toParameter program index = 
-        function
-        | '1' -> program |> peek index |> Immediate
-        | _ -> program |> peek index |> Position
-
-    let read program =
-        function 
-        | Immediate v -> v
-        | Position v -> program.Memory.[v]
-
-    let write program v = 
-        function
-        | Immediate p -> program.Memory.[p] <- v
-        | Position p -> program.Memory.[program.Memory.[p]] <- v
+let _ = runWitArgs p [939393]
 
 
-    let eval program = 
-        let instruction = 
-            program
-            |> peek 0
-            |> fun i -> i.ToString().PadLeft(5, '0')
-            |> Seq.toList
-            |> List.rev
-        match instruction with 
-        | '1'::_::m1::m2::m3::_ -> Add(toParameter program 1 m1, toParameter program 2 m2, toParameter program 3 m3)
-        | '2'::_::m1::m2::m3::_ -> Mult(toParameter program 1 m1, toParameter program 2 m2, toParameter program 3 m3)
-        | '3'::_::m1::_ -> Input (toParameter program 1 m1)
-        | '4'::_::m1::_ -> Output (toParameter program 1 m1)
-        | _ -> Halt
 
-    let exec program =
-        function
-        | Halt -> program
-        | Add (p1, p2, p3) -> 
-            write program ((read program p1) + (read program p2)) p3
-            { program with Pointer = program.Pointer + 4}
-        | Mult (p1, p2, p3) -> 
-            write program ((read program p1) * (read program p2)) p3
-            { program with Pointer = program.Pointer + 4}
-        | Output p ->
-            printfn "%i" (read program p)
-            { program with Pointer = program.Pointer + 2 }
-        | Input p ->
-            write program p 1
-            { program with Pointer = program.Pointer + 2 }
-
-    let load source = Empty
--- 2019/references.fsx
+++ 2019/references.fsx
@@ -1,3 +1,4 @@
 #r @"C:\Program Files\dotnet\packs\Microsoft.NETCore.App.Ref\3.0.0\ref\netcoreapp3.0\System.dll"
 #r @"C:\Program Files\dotnet\packs\Microsoft.NETCore.App.Ref\3.0.0\ref\netcoreapp3.0\netstandard.dll"
 #load @"Library.fs"
+#load @"IntCode.fs"
