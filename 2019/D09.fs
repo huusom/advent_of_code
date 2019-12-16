8c6379a604dcc7aa21615fa26d31c06c24498c4b
huusom <huusom@huusom.org>
2019-12-16 15:13:47 +0100

    Day 09 done

--- 2019/D09.fs
+++ 2019/D09.fs
@@ -1,16 +1,55 @@
 ﻿module Y2019.D09
 
-open IntCode
+open IntCode64
 open Xunit
+open Xunit.Abstractions
+open System.IO
 
-[<Fact>]
-let test() =
-    let expected = "109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99"
-    let program = 
-        expected
-        |> Program.load
-        |> Program.runWithTerm (Term.createBufferTerm Seq.empty)
-    
-    let actual = program.Memory |> Seq.map (sprintf "%i") |> String.concat ","
+type Tests(helper: ITestOutputHelper) =
+    [<Theory>]
+    [<InlineData("109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99", "109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99")>]
+    [<InlineData("1102,34915192,34915192,7,4,7,99,0", "1219070632396864")>]
+    [<InlineData("104,1125899906842624,99", "1125899906842624")>]
+    let examples(source, expected) =
 
-    Assert.Equal(expected, actual)
+        let program =
+            Program.run
+                { Program.Empty with
+                      Memory = Program.load source
+                      Term = Term.createBufferTerm Seq.empty
+                      Log = Some helper.WriteLine }
+
+        let actual =
+            program.Term.Output
+            |> Seq.map (sprintf "%i")
+            |> String.concat ","
+
+
+        Assert.Equal(expected, actual)
+
+
+    [<Fact>] 
+    let firstPuzzle () = 
+        let prg = 
+            Program.run
+                { Program.Empty with
+                    Memory = "input/d09.txt" |> File.ReadAllText |> Program.load
+                    Term = Term.createBufferTerm (Seq.singleton 1L) }
+
+        for x in prg.Term.Output do
+            helper.WriteLine(x.ToString())
+        
+        Assert.Equal(3063082071L, prg.Term.Output.Head)
+
+    [<Fact>] 
+    let secondPuzzle () = 
+        let prg = 
+            Program.run
+                { Program.Empty with
+                    Memory = "input/d09.txt" |> File.ReadAllText |> Program.load
+                    Term = Term.createBufferTerm (Seq.singleton 2L) }
+
+        for x in prg.Term.Output do
+            helper.WriteLine(x.ToString())
+        
+        Assert.Equal(81348L, prg.Term.Output.Head)
