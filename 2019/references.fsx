34ae3c51d11f7765b80d43c7c170b44c7e67b2a0
Christian Frank Huusom <huusom@huusom.org>
2020-02-06 00:26:48 +0100

    Day 14 done

--- 2019/references.fsx
+++ 2019/references.fsx
@@ -1,5 +1,10 @@
 #r @"C:\Program Files\dotnet\packs\Microsoft.NETCore.App.Ref\3.0.0\ref\netcoreapp3.0\System.dll"
 #r @"C:\Program Files\dotnet\packs\Microsoft.NETCore.App.Ref\3.0.0\ref\netcoreapp3.0\netstandard.dll"
+#r @"C:\Users\huusom\.nuget\packages\microsoft.testplatform.testhost\16.2.0\lib\netstandard1.5\testhost.dll"
+#r @"C:\Users\huusom\.nuget\packages\xunit.abstractions\2.0.2\lib\netstandard2.0\xunit.abstractions.dll"
+#r @"C:\Users\huusom\.nuget\packages\xunit.assert\2.4.0\lib\netstandard2.0\xunit.assert.dll"
+#r @"C:\Users\huusom\.nuget\packages\xunit.extensibility.core\2.4.0\lib\netstandard2.0\xunit.core.dll"
+#r @"C:\Users\huusom\.nuget\packages\xunit.extensibility.execution\2.4.0\lib\netstandard2.0\xunit.execution.dotnet.dll"
 #load @"Library.fs"
 #load @"IntCode64.fs"
 
