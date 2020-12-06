d3f417c26de630bad39ccedbc8d97af9fdb02ebd
Christian Frank Huusom <huusom@huusom.org>
2020-12-06 16:24:39 +0100

    2020/05

--- 2020/day-05.fs
+++ 2020/day-05.fs
@@ -8,18 +8,51 @@ open Lib
 System.Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 #endif
 
-let source = File.load 05 
+let source = File.load 05
 
 let puzzle_1 = 0
 
 let puzzle_2 = 0
 
+let upper (x, y) =
+    let h = (y - x + 1) / 2
+    (x + h, y)
+
+let lower (x, y) =
+    let h = (y - x) / 2
+    (x, x + h)
+
+let divide (r, c) =
+    function
+    | 'F' -> (lower r), c
+    | 'B' -> (upper r), c
+    | 'R' -> r, (upper c)
+    | _ -> r, (lower c)
+
+let seat_id line =
+    Seq.scan divide ((0, 127), (0, 7)) line
+    |> Seq.toList
+    |> Seq.last
+    |> fun x -> (x |> fst |> fst) * 8 + (x |> snd |> snd)
 
 
-let ``have source file`` () =  source |> Seq.isEmpty |> should equal false
+[<Fact>]
+let ``have source file`` () =
+    source |> Seq.isEmpty |> should equal false
 
+[<Fact>]
+let ``puzzle 1 is correct`` () =
+    source
+    |> Seq.map seat_id
+    |> Seq.max
+    |> should equal 0
 
-let ``puzzle 1 is correct`` () = puzzle_1 |> should equal 0
+[<Fact>]
+let ``puzzle 2 is correct`` () =
+    let seats =
+        source |> Seq.map seat_id |> Seq.sort |> Set.ofSeq
 
-
-let ``puzzle 2 is correct`` () = puzzle_2 |> should equal 0
+    [ 0 .. (seats.Count) ]
+    |> List.filter (fun i -> Set.contains i seats |> not)
+    |> List.last
+    |> should equal 636
