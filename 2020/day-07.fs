a2927fcd15366859564a8d525b57389973f47d5c
Christian Huusom <chu@siteimprove.com>
2020-12-07 13:26:16 +0100

    2020/07

--- 2020/day-07.fs
+++ 2020/day-07.fs
@@ -3,23 +3,75 @@ module Puzzles.day_07
 open FsUnit.Xunit
 open Xunit
 open Lib
+open System.Text.RegularExpressions
+open Strings
 
 #if INTERACTIVE
 System.Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 #endif
 
-let source = File.load 07 
+let source = File.load 07
 
-let puzzle_1 = 0
+let rx = Regex @"(\d+) (\w+ \w+) bags?"
 
-let puzzle_2 = 0
+let create_rule line =
+    let name =
+        line
+        / (line.IndexOf(' ', line.IndexOf(' ') + 1))
+        |> fst
+
+    let system =
+        rx.Matches(line)
+        |> Seq.map (fun m -> m.Groups.[2].Value, (int m.Groups.[1].Value))
+        |> Map.ofSeq
+
+    name, system
 
 
+let lookup rules name =
+    rules
+    |> Seq.filter (snd >> Map.containsKey name)
+    |> Seq.map fst
 
-let ``have source file`` () =  source |> Seq.isEmpty |> should equal false
+let rec find rules name =
+    let names = lookup rules name
+    names
+    |> Seq.map (find rules)
+    |> Seq.concat
+    |> Seq.append names
+    |> Seq.distinct
+
+let rec calc (rules: Map<string, Map<string, int>>) (name, value) =
+    match Map.tryFind name rules with
+    | None -> [ value ]
+    | Some children when children = Map.empty -> [ value ]
+    | Some children ->
+        let tl =
+            children
+            |> Map.toSeq
+            |> Seq.map (calc rules)
+            |> Seq.concat
+            |> Seq.map ((*) value)
+            |> Seq.toList
+
+        value :: tl
 
 
-let ``puzzle 1 is correct`` () = puzzle_1 |> should equal 0
+[<Fact>]
+let ``have source file`` () =
+    source |> Seq.isEmpty |> should equal false
 
+[<Fact>]
+let ``puzzle 1 is correct`` () =
+    let rules = source |> Seq.map create_rule
+    find rules "shiny gold"
+    |> Seq.length
+    |> should equal 197
 
-let ``puzzle 2 is correct`` () = puzzle_2 |> should equal 0
+[<Fact>]
+let ``puzzle 2 is correct`` () = 
+    let rules = source |> Seq.map create_rule |> Map.ofSeq
+    calc rules ("shiny gold", 1)
+    |> List.tail
+    |> List.sum
+    |> should equal 85324
