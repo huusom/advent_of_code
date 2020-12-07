470fa7119dbfce47a4519d0162cd21a8a8fe552a
Christian Huusom <chu@siteimprove.com>
2020-12-07 13:26:06 +0100

    Code clean-up

--- 2019/day-24.fs
+++ 2019/day-24.fs
@@ -15,11 +15,11 @@ let puzzle_1 = 0
 let puzzle_2 = 0
 
 
-[<Fact(Skip = "Not done yet")>]
+
 let ``have source file`` () = source |> should not' (be None)
 
-[<Fact(Skip = "Not done yet")>]
+
 let ``puzzle 1 is correct`` () = puzzle_1 |> should equal 0
 
-[<Fact(Skip = "Not done yet")>]
+
 let ``puzzle 2 is correct`` () = puzzle_2 |> should equal 0
