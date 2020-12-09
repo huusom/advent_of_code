3d47bfa5136b9edb9a8a0340b55f2e8bad9427f6
Christian Huusom <chu@siteimprove.com>
2020-12-09 15:10:16 +0100

    2020/09

--- 2020/day-09.fs
+++ 2020/day-09.fs
@@ -8,18 +8,58 @@ open Lib
 System.Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 #endif
 
-let source = File.load 09 
+let source = File.load 09
 
-let puzzle_1 = 0
+let pairs line =
+    let l = Array.length line - 1
+    let t = Array.get line l
 
-let puzzle_2 = 0
+    seq {
+        for a in 0 .. l do
+            for b in (a + 1) .. l do
+                let a' = line.[a]
+                let b' = line.[b]
+                if a' + b' = t then yield true
+    } 
+    |> Seq.exists id
 
+let find size input =
+    input
+    |> Seq.map int
+    |> Seq.windowed (size + 1)
+    |> Seq.skip (size + 1)
+    |> Seq.filter (pairs >> not)
+    |> Seq.head
+    |> Seq.last
 
+exception Found of int list
 
-let ``have source file`` () =  source |> Seq.isEmpty |> should equal false
+let rec folder t current next =
+    match List.sum current with
+    | 0 -> [ next ]
+    | i when i < t -> List.append current [ next ]
+    | i when i > t -> folder t (List.tail current) next
+    | t -> raise (Found current)
 
+let search target input =
+    try
+        input
+        |> Seq.map int
+        |> Seq.fold (folder target) []
+        |> List.sum
+    with Found values ->
+        let l = values |> List.sort
+        (List.head l) + (List.last l)
 
-let ``puzzle 1 is correct`` () = puzzle_1 |> should equal 0
+[<Fact>]
+let ``have source file`` () =
+    source |> Seq.isEmpty |> should equal false
 
+[<Fact>]
+let ``puzzle 1 is correct`` () =
+    source |> find 25 |> should equal 14360655
 
-let ``puzzle 2 is correct`` () = puzzle_2 |> should equal 0
+[<Fact>]
+let ``puzzle 2 is correct`` () =
+    source |> search 14360655 |> should equal 1962331
+
