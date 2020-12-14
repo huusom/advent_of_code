12d9ba600a13f06efda9dd6903480aff6a0fff84
Christian Huusom <chu@siteimprove.com>
2020-12-14 11:40:50 +0100

    2020/10

--- 2020/day-10.fs
+++ 2020/day-10.fs
@@ -1,66 +1,15 @@
 module aoc2020.day_10
 
 #if INTERACTIVE
+#load @"..\Lib\references.fsx"
 System.Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 #endif
 
 open FsUnit.Xunit
 open Xunit
 
-
-#if INTERACTIVE
-#load @"..\Lib\references.fsx"
-System.Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
-#endif
-
 let source =
     File.load 10 |> Seq.map (int) |> Set.ofSeq
-
-let data_1 =
-    [ 16
-      10
-      15
-      5
-      1
-      11
-      7
-      19
-      6
-      12
-      4 ]
-
-let data_2 =
-    [ 28
-      33
-      18
-      42
-      31
-      14
-      46
-      20
-      48
-      47
-      24
-      23
-      49
-      45
-      19
-      38
-      39
-      11
-      1
-      32
-      25
-      35
-      8
-      17
-      7
-      9
-      4
-      2
-      34
-      10
-      3 ]
 
 let prep input =
     let m = Seq.max input
@@ -71,16 +20,30 @@ let prep input =
     }
     |> Seq.sort
 
-let diffs input = 
+let diffs input =
     input
     |> Seq.pairwise
     |> Seq.map (fun (a, b) -> b - a)
 
+let to_string input =
+    input |> Seq.map (string) |> String.concat ""
 
+let to_array str =
+    System.Text.RegularExpressions.Regex.Split(str, "3+")
+    |> Seq.filter (System.String.IsNullOrEmpty >> not)
+
+let to_int64 = 
+    function 
+    | "1111" -> 7L
+    | "111" -> 4L
+    | "11" -> 2L
+    | _ -> 1L
+
+[<Fact>]
 let ``have source file`` () =
     source |> Seq.isEmpty |> should equal false
 
-
+[<Fact>]
 let ``puzzle 1 is correct`` () =
     source
     |> prep
@@ -90,5 +53,16 @@ let ``puzzle 1 is correct`` () =
     |> Seq.reduce (*)
     |> should equal 2210
 
+[<Fact>]
+let ``puzzle 2 is correct`` () =
+    source
+    |> prep
+    |> diffs
+    |> to_string
+    |> to_array
+    |> Seq.map to_int64
+    |> Seq.reduce (*)
+    |> should equal 7086739046912L
 
-let ``puzzle 2 is correct`` () = 0 |> should equal 0
+
+
