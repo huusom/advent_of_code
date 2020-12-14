fcc9e2d656cb3e53a8f5dc301f26e6d99fcd99b2
Christian Huusom <chu@siteimprove.com>
2020-12-14 21:28:15 +0100

    2020/14

--- 2020/day-11.fs
+++ 2020/day-11.fs
@@ -1,13 +1,12 @@
 module aoc2020.day_11
 
-open FsUnit.Xunit
-open Xunit
-
-
 #if INTERACTIVE
 #load @"..\Lib\references.fsx"
 System.Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 #endif
+
+open FsUnit.Xunit
+open Xunit
 
 let source = File.load 11 
 
@@ -15,12 +14,8 @@ let puzzle_1 = 0
 
 let puzzle_2 = 0
 
-
-
 let ``have source file`` () =  source |> Seq.isEmpty |> should equal false
 
-
 let ``puzzle 1 is correct`` () = puzzle_1 |> should equal 0
-
 
 let ``puzzle 2 is correct`` () = puzzle_2 |> should equal 0
--- 2020/day-14.fs
+++ 2020/day-14.fs
@@ -1,26 +1,106 @@
 module aoc2020.day_14
 
-open FsUnit.Xunit
-open Xunit
-
-
 #if INTERACTIVE
 #load @"..\Lib\references.fsx"
 System.Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 #endif
 
-let source = File.load 14 
+open FsUnit.Xunit
+open Xunit
+open Strings
 
-let puzzle_1 = 0
+let source = File.load 14
 
-let puzzle_2 = 0
+//let int64 (s:string) = System.Convert.ToInt64 s
+
+type Cmd =
+    | Mask of string
+    | Mem of int64 * int64
+
+let parse line =
+    match line with
+    | Rx "mask = (\w+)" [ l ] -> Mask l
+    | Rx "mem\[(\d+)\] = (\d+)" [ a; i ] -> Mem((int64 a), (int64 i))
+
+let value_bitmask m i =
+    match m with
+    | '1' -> '1'
+    | '0' -> '0'
+    | _ -> i
+
+let address_bitmask m i =
+    match m with
+    | '0' -> i
+    | '1' -> '1'
+    | x -> x
+
+let to_string (i: int64) =
+    System.Convert.ToString(i, 2).PadLeft(36, '0')
+
+let to_int64 s = System.Convert.ToInt64(s, 2)
+
+let address_mask mask addr =
+    addr
+    |> to_string
+    |> Seq.map2 address_bitmask mask
+    |> Seq.toList
+
+let rec unravel chars =
+    let add a lst = lst |> List.map (fun lst' -> a :: lst')
+    match chars with
+    | [] -> [ [ ' ' ] ]
+    | 'X' :: rst -> 
+        let lst = unravel rst
+        List.append (add '0' lst) (add '1' lst)
+    | hd :: rst -> add hd (unravel rst)
+
+let add_addresses num mask addr dict = 
+    addr 
+    |> address_mask mask
+    |> unravel
+    |> List.map (fun l -> System.String(List.toArray l, 0, 36) |> to_int64)
+    |> List.fold (fun d a-> Map.add a num d) dict 
+ 
+let value_mask num mask addr dict =
+    let v =
+        num
+        |> to_string
+        |> Seq.map2 value_bitmask mask
+        |> Seq.toArray
+        |> System.String
+        |> to_int64
+
+    Map.add addr v dict
 
 
+let exec calc (m, dict) =
+    function
+    | Mask s -> (s, dict)
+    | Mem (a, i) -> (m, calc i m a dict)
 
-let ``have source file`` () =  source |> Seq.isEmpty |> should equal false
 
+[<Fact>]
+let ``have source file`` () =
+    source |> Seq.isEmpty |> should equal false
 
-let ``puzzle 1 is correct`` () = puzzle_1 |> should equal 0
+[<Fact>]
+let ``puzzle 1 is correct`` () =
+    source
+    |> Seq.map parse
+    |> Seq.fold (exec value_mask) ("", Map.empty)
+    |> snd
+    |> Map.toSeq
+    |> Seq.map snd
+    |> Seq.reduce (+)
+    |> should equal 10885823581193L
 
-
-let ``puzzle 2 is correct`` () = puzzle_2 |> should equal 0
+[<Fact>]
+let ``puzzle 2 is correct`` () = 
+    source
+    |> Seq.map parse
+    |> Seq.fold (exec add_addresses) ("", Map.empty)
+    |> snd
+    |> Map.toSeq
+    |> Seq.map snd
+    |> Seq.reduce (+)
+    |> should equal 3816594901962L
