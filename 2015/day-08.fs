bdc674ff5f3709a09246db501596744abe0c9086
Christian Huusom <chu@siteimprove.com>
2020-12-26 00:11:59 +0100

    2015/08

--- 2015/day-08.fs
+++ 2015/day-08.fs
@@ -1,26 +1,45 @@
 module aoc2015.day_08
 
-open FsUnit.Xunit
-open Xunit
-
-
 #if INTERACTIVE
 #load @"..\Lib\references.fsx"
 System.Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 #endif
 
-let source = File.load 08 
+open FsUnit.Xunit
+open Xunit
+open System.Text.RegularExpressions
 
-let puzzle_1 = 0
+let source = File.load 08
 
-let puzzle_2 = 0
+let rx = Regex @"\\x[a-f0-9][a-f0-9]|\\""|\\\\" 
+
+let replace line = rx.Replace(line, "_")
+
+let count_1 line = 
+    let l2 = replace line |> String.length
+    l2 - 2
+
+let count_2 (line:string) =
+    line
+        .Replace("\\", "\\\\")
+        .Replace("\"", "\\\"")
+    |> String.length
+    |> (+) 2
 
 
+[<Fact>]
+let ``have source file`` () =
+    source |> Seq.isEmpty |> should equal false
 
-let ``have source file`` () =  source |> should not' (be None)
+[<Fact>]
+let ``puzzle 1 is correct`` () = 
+    let l1 = source |> Seq.sumBy String.length
+    let l2 = source |> Seq.sumBy count_1
+    l1 - l2 |> should equal 1371
 
+[<Fact>]
+let ``puzzle 2 is correct`` () =
+    let l1 = source |> Seq.sumBy String.length
+    let l2 = source |> Seq.sumBy count_2
+    l2 - l1 |> should equal 2117
 
-let ``puzzle 1 is correct`` () = puzzle_1 |> should equal 0
-
-
-let ``puzzle 2 is correct`` () = puzzle_2 |> should equal 0
