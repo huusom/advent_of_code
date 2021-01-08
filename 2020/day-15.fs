764dc02cced79130e2d6499465c284bdca045ad1
Christian Huusom <chu@siteimprove.com>
2021-01-08 09:31:56 +0100

    2020/15

--- 2020/day-15.fs
+++ 2020/day-15.fs
@@ -1,26 +1,78 @@
 module aoc2020.day_15
 
-open FsUnit.Xunit
-open Xunit
-
-
 #if INTERACTIVE
 #load @"..\Lib\references.fsx"
 System.Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 #endif
 
-let source = File.load 15 
+open FsUnit.Xunit
+open Xunit
 
-let puzzle_1 = 0
+let source = "12,20,0,6,1,17,7"
 
-let puzzle_2 = 0
-
-
-
-let ``have source file`` () =  source |> Seq.isEmpty |> should equal false
-
-
-let ``puzzle 1 is correct`` () = puzzle_1 |> should equal 0
+type Game =
+    { turn: int
+      spoken: int
+      history: Map<int, int list> }
 
 
-let ``puzzle 2 is correct`` () = puzzle_2 |> should equal 0
+let parse input =
+    let data = input |> Strings.split ','
+
+    { turn = data.Length
+      spoken = data |> Array.last |> int
+      history =
+          data
+          |> Seq.mapi (fun i s -> (int s), [ (i + 1) ])
+          |> Map.ofSeq }
+
+let speek game =
+    match Map.tryFind game.spoken game.history with
+    | None -> 0
+    | Some l ->
+        match l with
+        | []
+        | [ _ ] -> 0
+        | t1 :: t0 :: _ -> t1 - t0
+
+let insert game num =
+    let t = game.turn + 1
+
+    match Map.tryFind num game.history with
+    | None ->
+        { spoken = num
+          turn = t
+          history = Map.add num [ t ] game.history }
+    | Some (t0 :: _) ->
+        { spoken = num
+          turn = t
+          history = Map.add num [ t; t0 ] game.history }
+
+let rec run steps game =
+    if steps <= game.turn then
+        game.spoken
+    else
+        game |> speek |> insert game |> run steps
+
+
+let ``have source file`` () =
+    source |> Seq.isEmpty |> should equal false
+
+
+
+[<Fact>]
+let ``puzzle 1 is correct`` () =
+    source |> parse |> run 2020 |> should equal 866
+
+
+[<Fact(Skip = "Long running")>]
+let ``puzzle 2 is correct`` () =
+    source
+    |> parse
+    |> run 30000000
+    |> should equal 1437692
+
+[<Fact>]
+let ``first set of tests`` () =
+    parse "1,3,2" |> run 2020 |> should equal 1
+    parse "2,1,3" |> run 2020 |> should equal 10
